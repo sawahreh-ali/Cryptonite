680d0b8aec33b12ebd3e931ba02d253909a03e65
Ali Sawahreh <sawahreh-ali@hotmail.com>
2020-02-26 21:40:20 +0200

    add switch button

--- js/home-view.js
+++ js/home-view.js
@@ -5,30 +5,45 @@ var homeView = function(data) {
         innerHtml += 
         `
         <div class="row no-gutters">
-        <div class="col-lg-4">
+        <div class="col-md-4">
           <div class="card h-100">
             <div class="card-body">
               <h5 class="card-title">${data[i].symbol}</h5>
+              <label class="switch switch-flat">
+                <input class="switch-input" type="checkbox" />
+                <span class="switch-label" data-on="On" data-off="Off"></span> 
+                <span class="switch-handle"></span> 
+              </label>
               <p class="card-text">${data[i].name}</p>
-              <button class="btn btn-primary">Go somewhere</button>
+              <button class="btn btn-primary">More Info...</button>
             </div>
           </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-md-4">
           <div class="card h-100">
             <div class="card-body">
               <h5 class="card-title">${data[i + 1].symbol}</h5>
+              <label class="switch switch-flat">
+                <input class="switch-input" type="checkbox" />
+                <span class="switch-label" data-on="On" data-off="Off"></span> 
+                <span class="switch-handle"></span> 
+              </label>
               <p class="card-text">${data[i + 1].name}</p>
-              <button class="btn btn-primary">Go somewhere</button>
+              <button class="btn btn-primary">More Info...</button>
             </div>
           </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-md-4">
           <div class="card h-100">
               <div class="card-body">
                   <h5 class="card-title">${data[i + 2].symbol}</h5>
+                  <label class="switch switch-flat">
+                    <input class="switch-input" type="checkbox" />
+                    <span class="switch-label" data-on="On" data-off="Off"></span> 
+                    <span class="switch-handle"></span> 
+                  </label>
                   <p class="card-text">${data[i + 2].name}</p>
-                  <button class="btn btn-primary">Go somewhere</button>
+                  <button class="btn btn-primary">More Info...</button>
               </div>
           </div>
         </div>
